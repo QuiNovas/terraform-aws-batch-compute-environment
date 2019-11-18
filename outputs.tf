8179c536424a642630ae8bd65d1ff2643a366aa3
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2019-11-18 20:26:46 +0530

    interpolation changes

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "batch_service_role_arn" {
-  value = "${join("", aws_iam_role.aws_batch_service_role.*.arn)}"
+  value = join("", aws_iam_role.aws_batch_service_role.*.arn)
 }
 
 output "arn" {
@@ -12,29 +12,29 @@ output "ecs_cluster_arn" {
 }
 
 output "ecs_instance_profile_arn" {
-  value = "${join("", aws_iam_role.ecs_instance_role.*.arn)}"
+  value = join("", aws_iam_role.ecs_instance_role.*.arn)
 }
 
 output "ecs_instance_role_arn" {
-  value = "${join("", aws_iam_role.ecs_instance_role.*.arn)}"
+  value = join("", aws_iam_role.ecs_instance_role.*.arn)
 }
 
 output "public_subnet_ids" {
-  value = "${join("", aws_subnet.public.*.id)}"
+  value = join("", aws_subnet.public.*.id)
 }
 
 output "private_subnet_ids" {
-  value = "${join("", aws_subnet.private.*.id)}"
+  value = join("", aws_subnet.private.*.id)
 }
 
 output "security_group_id" {
-  value = "${join("", aws_security_group.base_sg.*.id)}"
+  value = join("", aws_security_group.base_sg.*.id)
 }
 
 output "spot_fleet_role_arn" {
-  value = "${join("", aws_iam_role.spot_fleet_role.*.arn)}"
+  value = join("", aws_iam_role.spot_fleet_role.*.arn)
 }
 
 output "vpc_id" {
-  value = "${join("", aws_vpc.vpc.*.id)}"
+  value = join("", aws_vpc.vpc.*.id)
 }
